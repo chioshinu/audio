da610f224b03df8478080490abc6db4e93bc28a7
Sergey Gychka <sergey.gychka@masterofcode.com>
2013-08-09 12:16:55 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audio::Application.config.secret_token = '3367147757235f8001339b119295d013e21da636126b5b81d3fc0512cf06de3b9910813b8831202727b2d2e6f8ab0082fd0baa20b10aff2a45320889ab83b998'
